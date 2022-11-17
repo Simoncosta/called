eb0226c49fec47d6792a4178673d59dd5783fa9d
Simon F. Costa <saimom_costa@hotmail.com>
2022-11-17 23:04:51 +0000

    ajuste

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,41 @@
+import { useState } from "react";
+import { FiMessageSquare, FiPlus } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import Breadcrumbs from "../../components/Breadcrumbs";
 import Header from "../../components/Header";
+import "./dashboard.css";
 
 export function Dashboard() {
+
+    const [chamados, setChamados] = useState([])
 
     return(
         <div>
 
             <Header />
 
-            <h1>DASHBOARD</h1>
+            <div className="content">
+                <Breadcrumbs name="Chamados">
+                    <FiMessageSquare size={25} />
+                </Breadcrumbs>
+
+                {chamados.length === 0 ? (
+                    <div className="container dashboard">
+                        <span>Nenhum chamado registrado...</span>
+                        <Link to="/new" className="new">
+                            <FiPlus size={25} color="#FFF" />
+                            Novo Chamado
+                        </Link>
+                    </div>
+                ):(
+                    <>
+                        <Link to="/new" className="new">
+                            <FiPlus size={25} color="#FFF" />
+                            Novo Chamado
+                        </Link>
+                    </>
+                )}
+            </div>
         </div>
     );
 }
